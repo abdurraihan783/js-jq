c196ef7793f4d0f714827f322669ab8130544627
abdurraihan783 <abdurraihanrony783@gmail.com>
2024-11-23 22:03:11 +0600

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,44 @@
+// jQ
+$(function(){
+
+  $('.counter').counterUp({
+    delay: 10,
+    time: 1000
+
+    });   
+});
+
+
+
+
+
+
+
+
+
+
+
+
+// js
+wow = new WOW(
+    {
+    boxClass:     'wow',      // default
+    animateClass: 'animated', // default
+    offset:       0,          // default
+    mobile:       true,       // default
+    live:         true        // default
+}
+)
+wow.init();
+
+
+
+
+// function.ready({
+  var mixer = mixitup('#hello', {
+     animation: {
+     duration: 300
+     }
+
+});
+// });
